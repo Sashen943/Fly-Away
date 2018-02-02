8344532f03d284d763b6182590bad11714304296
Sashen <sashenpillay943@gmail.com>
2018-02-02 13:12:43 +0200

    Files removed

--- app/src/main/java/za/co/dvt/spillay/flyaway/database/Flight.java
+++
@@ -1,8 +0,0 @@
-package za.co.dvt.spillay.flyaway.database;
-
-/**
- * Created by SPillay on 2018/01/26.
- */
-
-public class Flight {
-}
--- app/src/main/java/za/co/dvt/spillay/flyaway/repository/FlightRepositoryImp.java
+++
@@ -1,8 +0,0 @@
-package za.co.dvt.spillay.flyaway.repository;
-
-/**
- * Created by SPillay on 2018/01/29.
- */
-
-public class FlightRepositoryImp {
-}
